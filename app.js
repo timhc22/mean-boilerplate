ce16efb50243460051fd5dde0685679f3f76ef05
Timothy Coleman <timhc22@gmail.com>
2016-08-31 19:44:20 +0100

    Test for server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World, I am heress!');
+    res.send('Hello World, I am heresss!');
 });
 
 app.listen(8080, function () {
