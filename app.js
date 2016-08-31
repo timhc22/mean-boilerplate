ddf5d22f07b320193385e10a46104742f9cf99bd
Timothy Coleman <timhc22@gmail.com>
2016-08-31 01:16:53 +0100

    Change port for app

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World\n');
+// }).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
+// console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
