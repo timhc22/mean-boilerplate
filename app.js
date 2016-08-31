a4cf9fba67f12c9a32e86f169ba43c92b3668c5c
Timothy Coleman <timhc22@gmail.com>
2016-08-31 19:53:46 +0100

    Add test for server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World, I am heresssss!');
+    res.send('Hello World, I am heressssss!');
 });
 
 app.listen(8080, function () {
