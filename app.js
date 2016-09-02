4640854f6b999693609fec83606cb65f2e2b3eb9
Timothy Coleman <timhc22@gmail.com>
2016-09-02 14:03:38 +0100

    Remove trailing esses

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World, I am heressssss!');
+    res.send('Hello World, I am heres!');
 });
 
 app.listen(8080, function () {
